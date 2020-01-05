49c1c7f187cb17e7a318a3c148f59ad41431fdd1
Gottfried Eibner <go@ybna.net>
2020-01-05 14:48:42 +0100

    merged enemy script

--- Space Opera Unity/Assets/Main/Scripts/Enemies/Enemy.cs
+++ Space Opera Unity/Assets/Main/Scripts/Enemies/Enemy.cs
@@ -6,10 +6,6 @@ public class Enemy : MonoBehaviour
 {
 	public int HitPoints = 1;
 	public int ScoreValue = 1;
-<<<<<<< HEAD
-=======
-	public float DelayAnimation = 0f;
->>>>>>> 59027e5ade3b96f47a0bc3b96ac265d746186da7
 	public UnityEvent OnDeath = new UnityEvent();
 
 	private Shootable _shootable;
@@ -22,24 +18,9 @@ public class Enemy : MonoBehaviour
 		_shootable._onShot.AddListener(OnShot);
 
 		_animator = GetComponent<Animator>();
-<<<<<<< HEAD
-	}
-
-    private void OnShot()
-=======
-		_animator.enabled = false;
-		Invoke("DelayAnimationCR", DelayAnimation);
-
-		_isAlive = true;
-	}
-
-	private void DelayAnimationCR()
-	{
-		_animator.enabled = true;
 	}
 
 	private void OnShot()
->>>>>>> 59027e5ade3b96f47a0bc3b96ac265d746186da7
 	{
 		if (!_isAlive) return;
 
