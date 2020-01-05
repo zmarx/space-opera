d31701f6e17aafa36bfd009260ed5ed2e214d9d3
Gottfried Eibner <go@ybna.net>
2020-01-05 14:46:09 +0100

    readjusted attack and die animations (so that position zero is lowest point), reduced oktoboss prefab parts, assigend oktoboss animation controller

---
+++ Space Opera Unity/Assets/Main/Scripts/AnimationDelay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class AnimationDelay : MonoBehaviour
+{
+    public float DelayAnimation = 0f;
+
+    private Animator []_animators;
+    private void Start()
+    {
+        _animators = GetComponentsInChildren<Animator>(true);
+        EnableAnimators(false);
+        Invoke("DelayAnim", DelayAnimation);
+    }
+
+    private void EnableAnimators(bool enable)
+    {
+        foreach (var ani in _animators)
+            ani.enabled = enable;
+    }
+
+    private void DelayAnim()
+    {
+        EnableAnimators(true);
+    }
+}
--- Space Opera Unity/Assets/Main/Scripts/Enemies/Enemy.cs
+++ Space Opera Unity/Assets/Main/Scripts/Enemies/Enemy.cs
@@ -6,7 +6,6 @@ public class Enemy : MonoBehaviour
 {
 	public int HitPoints = 1;
 	public int ScoreValue = 1;
-    public float DelayAnimation = 0f;
 	public UnityEvent OnDeath = new UnityEvent();
 
 	private Shootable _shootable;
@@ -18,14 +17,7 @@ public class Enemy : MonoBehaviour
 		_shootable._onShot.AddListener(OnShot);
 
 		_animator = GetComponent<Animator>();
-        _animator.enabled = false;
-        Invoke("DelayAnimationCR", DelayAnimation);
 	}
-
-    private void DelayAnimationCR()
-    {
-        _animator.enabled = true;
-    }
 
     private void OnShot()
 	{
